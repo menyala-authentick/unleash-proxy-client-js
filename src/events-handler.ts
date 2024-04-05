cbda4c97d466519557cb3d4c67b69ae6892100e4
Ivan Kuzmin <kuzmin6969@gmail.com>
2024-04-05 13:42:00 +0300

    Replace uuid library with naive UUID implementation

--- src/events-handler.ts
+++ src/events-handler.ts
@@ -1,9 +1,25 @@
-import { v1 as uuidv1 } from 'uuid';
 import { IContext } from '.';
+
+/** Generates UUID v4
+ *
+ * @node There is a bug in Chrome's Math.random() according to http://devoluk.com/google-chrome-math-random-issue.html
+ *       For that reason we use Date.now() as well.
+ */
+function uuid(): string {
+    function s(n: number) { return h((Math.random() * (1<<(n<<2)))^Date.now()).slice(-n); }
+    function h(n: number) { return (n|0).toString(16); }
+    return  [
+        s(4) + s(4), s(4),
+        '4' + s(3),                    // UUID version 4
+        h(8|(Math.random()*4)) + s(3), // {8|9|A|B}xxx
+        // s(4) + s(4) + s(4),
+        Date.now().toString(16).slice(-10) + s(2) // Use timestamp to avoid collisions
+    ].join('-');
+}
 
 class EventsHandler {
     private generateEventId() {
-        return uuidv1();
+        return uuid();
     }
 
     public createImpressionEvent(
